79ec97385958e63dd6e5cc6288dd2b409a025acc
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-11-21 22:20:55 +0100

    added tests for "any custom component class extends from any dom element"

--- test/component.spec.js
+++ test/component.spec.js
@@ -4,13 +4,26 @@ import { component } from 'src/app-decorators';
 describe('@component', () => {
 
 	@component(HTMLElement)
-	class FooComponent {
+	class FooComponent {}
 
-	}
+	@component(HTMLFormElement)
+	class BarComponent {}
 
 	it('should instance of HTMLElement', () => {
 		let fooComponent = new FooComponent();
 		fooComponent.should.be.instanceOf(HTMLElement);
+	});
+
+	it('should instance of HTMLFormElement', () => {
+		let barComponent = new BarComponent();
+		barComponent.should.be.instanceOf(HTMLFormElement);
+	});
+
+	it('should throw an exeption', () => {
+		(function(){
+			@component()
+			class BazComponent {}
+		}).should.throw('Passed argument is undefined');;
 	});
 
 });
