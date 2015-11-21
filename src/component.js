32718dbff18ce00277ba36e9b771c11d8213494b
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-11-21 22:21:46 +0100

    added implementation for "any custom component class extends from any dom element"

--- src/component.js
+++ src/component.js
@@ -1,11 +1,32 @@
 
 /**
+ * Inherit
+ *
+ * @param  {Function} Child
+ * @param  {Element} Parent
+ * @return {undefined}
+ */
+function inherit(Child: Funtion, Parent: Element) {
+	let F = function() {};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.prototype.constructor = Child;
+}
+
+/**
  * Component (CustomElement)
+ *
  * @param  {Any} ...args
  * @return {Function}
  */
-export default function component(...args) {
-	return function(target, name, descriptor) {
+export default function component(Element) {
 
+	if (Element === void 0) {
+		throw Error(`Passed argument is undefined`);
 	}
+
+	return function decorator(Component) {
+		inherit(Component, Element);
+	}
+
 }
