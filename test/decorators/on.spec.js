3afed0d22c97a9923ee4fe57426ef995b49647be
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-01-20 22:20:04 +0100

    test/decorators/on.spec.js: added tests for [at]on decorators

--- test/decorators/on.spec.js
+++ test/decorators/on.spec.js
@@ -13,4 +13,38 @@ describe('@on decorator', () => {
 
 	});
 
+	describe('@on decorator', () => {
+
+		it('should build correct property path of Eventhandler.prototype.$appDecorators.on.events', () => {
+
+			class Eventhandler {
+
+				@on('click .a') onClick_a() {}
+				@on('click .b .c') onClick_bc() {}
+
+				@on('mouseup .d') onMouseup_d(){}
+				@on('mouseup .e .f') onMouseup_ef(){}
+
+				@on('mousedown') onMousedown(){}
+
+			}
+
+			let events = Eventhandler.prototype.$appDecorators.on.events;
+			let prototype = Eventhandler.prototype;
+
+			// test property paths of clicks
+			events.should.have.propertyByPath("click .a").eql(prototype.onClick_a);
+			events.should.have.propertyByPath("click .b .c").eql(prototype.onClick_bc);
+
+			// test property paths of mouseup
+			events.should.have.propertyByPath("mouseup .d").eql(prototype.onMouseup_d);
+			events.should.have.propertyByPath("mouseup .e .f").eql(prototype.onMouseup_ef);
+
+			// test property paths of onMousedown
+			events.should.have.propertyByPath("mousedown").eql(prototype.onMousedown);
+
+		});
+
+	});
+
 });
