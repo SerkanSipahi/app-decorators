debb58c4a3bf6af9fa1efd6b449e4d947e3a9bf8
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-02-06 21:40:03 +0100

    decorators/on.spec.js: refactored tests; add integrationtest

--- test/decorators/on.spec.js
+++ test/decorators/on.spec.js
@@ -13,11 +13,23 @@ describe('@on decorator', () => {
 
 	});
 
-	describe('@on decorator', () => {
+	describe('@on decorator integrationtest with @component, @view and @on', () => {
 
-		it('should build correct property path of Eventhandler.prototype.$appDecorators.on.events', () => {
+		it('should work in combination', () => {
 
-			class Eventhandler {
+			@view(`<div class="foo">Foo</div>`)
+			@component(HTMLElement)
+			class Snack {
+
+				clickFooFuncCalled = false;
+
+				doSomething(arg){
+					this.clickFooFuncCalled = arg;
+				}
+
+				@on('click .foo') clickFoo(){
+					this.doSomething(true);
+				}
 
 				@on('click .a') onClick_a() {}
 				@on('click .b .c') onClick_bc() {}
@@ -29,8 +41,8 @@ describe('@on decorator', () => {
 
 			}
 
-			let events = Eventhandler.prototype.$appDecorators.on.events;
-			let prototype = Eventhandler.prototype;
+			let events = Snack.prototype.$appDecorators.on.events;
+			let prototype = Snack.prototype;
 
 			// test property paths of clicks
 			events.should.have.propertyByPath("click .a").eql(prototype.onClick_a);
@@ -42,6 +54,10 @@ describe('@on decorator', () => {
 
 			// test property paths of onMousedown
 			events.should.have.propertyByPath("mousedown").eql(prototype.onMousedown);
+
+			let snack = Snack.create();
+			snack.querySelector('.foo').click();
+			snack.clickFooFuncCalled.should.be.true();
 
 		});
 
