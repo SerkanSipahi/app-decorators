2088dd6307f505efe4e19e20cc6cadc0e9979843
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-02-26 22:25:13 +0100

    added idea/proposal for router usage

--- test/libs/router.spec.js
+++ test/libs/router.spec.js
@@ -11,4 +11,49 @@ describe('Class Router', () => {
 
 	});
 
+	it.skip('just an idea', () => {
+
+		let router = Router.create({
+			routes : {
+				'product::detail': {
+					'/product/detail/{{ id }}' : () => {
+
+					}
+				},
+				'product::site': {
+					'/product/detail/{{ id }}?foo={{ a }}&bar={{ b }}#baz={{ c }}': ({ id, b, c }) => {
+
+					}
+				},
+				'checkout::login': {
+					'*?thats={{ it }}*': ({ it }) => {
+
+					}
+				},
+				'checkout::logout': {
+					'/logout(?:token=([0-9])+)?/': ([ number ]) => {
+
+					}
+				},
+			}
+		});
+
+		router.on('product::detail', '/product/detail/{{ id }}', ({ id }) => {
+
+		});
+
+		router.on('product::site', '/product/detail/{{ id }}?foo={{ a }}&bar={{ b }}#baz={{ c }}', ({ id, b, c }) => {
+
+		});
+
+		router.on('checkout::login', '*?thats={{ it }}*', ({ it }) => {
+
+		});
+
+		router.on('checkout::login', /.*logout(?:token=([0-9])+)?/, ([ number ]) => {
+
+		});
+
+	});
+
 });
