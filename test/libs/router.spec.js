f3826e08ab918680d1aace354fb9156f5ada875e
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-03-07 19:37:13 +0100

    Router: implemented test for urlchange behavior

--- test/libs/router.spec.js
+++ test/libs/router.spec.js
@@ -16,6 +16,75 @@ describe('Class Router', () => {
 
 	});
 
+	describe('on created new instance', () => {
+
+		let element = null;
+
+		beforeEach(() => {
+
+			element = document.createElement("div");
+			element.classList.add('anchor-container');
+			element.innerHTML = `
+				<a class="foo" href="/index"> Index </a>
+				<a class="bar" href="/index/details"> Details </a>
+				<a class="baz" href="/index/details?a=1&b=2"> Params </a>
+				<a class="qux" href="/index/details?a=1&b=2#c=3;d=4"> Params </a>
+			`;
+			document.body.appendChild(element);
+
+		});
+
+		afterEach(() => {
+			document.body.removeChild(element);
+		});
+
+		it('should trigger "urlchange" on pushState or on click of an a-link', () => {
+
+			// setup
+			let count = 0;
+			let fragment = null;
+			let router = Router.create({
+				config: {
+					Eventhandler,
+				}
+			});
+			sinon.spy(router, '_onUrlchange');
+			router.onUrlchange( event => {
+				fragment = event.detail.fragment;
+				++count;
+			});
+			// TODO: implment it with promise
+			// router.on('urlchange').then((event) => {
+			//
+			// });
+
+			// run test
+			element.querySelector('.foo').click();
+			count.should.equal(1);
+			element.querySelector('.bar').click();
+			count.should.equal(2);
+			element.querySelector('.baz').click();
+			count.should.equal(3);
+
+			element.querySelector('.qux').click();
+			count.should.equal(4);
+			element.querySelector('.qux').click();
+			count.should.equal(4);
+
+			// test popstate
+			// TODO: in chrome router.back() does not work
+			router.back();
+			router.back();
+			router.back();
+
+			// cleanup
+			// TODO: cleanup failed
+			// router.destroy(); FAILED
+
+		});
+
+	});
+
 	it.skip('just an idea', () => {
 
 		let router = Router.create({
