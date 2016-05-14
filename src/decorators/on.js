893bf36e29fc142e20eec2bcbb4b54e3f6e2ce36
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-05-14 23:28:50 +0200

    src/decorators/on.js: refactored

--- src/decorators/on.js
+++ src/decorators/on.js
@@ -17,12 +17,11 @@ export default function on(eventDomain) {
 
 		// register namespaces
 		on.helper.registerNamespaces(target);
-
 		// register events
 		on.helper.registerEvent(target, eventDomain, descriptor.value);
 
 		/**
-		 * ### Ensure "registerOnCreatedCallback" (see below) registered only once ###
+		 * ### Ensure "registerCallback('created', ..." (see below) registered only once ###
 		 * This function will called every time if an event registered e.g. @on('click .foo')
 		 * but registerOnCreatedCallback can only call once because we want only Create
 		 * one eventhandler
@@ -31,7 +30,7 @@ export default function on(eventDomain) {
 			return;
 		}
 
-		on.helper.registerOnCreatedCallback(target, ( domNode ) => {
+		on.helper.registerCallback('created', target, ( domNode ) => {
 
 			let eventHandler = Eventhandler.create({
 				events: domNode.$appDecorators.on.events,
@@ -45,7 +44,11 @@ export default function on(eventDomain) {
 
 		});
 
-		on.helper.registerOnDetachedCallback(target, (domNode) => {
+		on.helper.registerCallback('attached', target, (domNode) => {
+
+		});
+
+		on.helper.registerCallback('detached', target, (domNode) => {
 
 			// cleanup: remove all eventhandler
 			domNode.$.eventHandler.reset();
@@ -60,95 +63,69 @@ export default function on(eventDomain) {
  * ########## Decorator Helper ###########
  *****************************************/
 
-// define namespace
-on.helper = {};
+on.helper = {
+	/**
+	 * Helper for registering events
+	 * @param  {String} event
+	 * @param  {Function} callback
+	 * @return {Function} target
+	 */
+	registerEvent: (target, eventDomain, callback = function(){}) => {
 
-/**
- * Helper for registering events
- * @param  {String} event
- * @param  {Function} callback
- * @return {Function} target
- */
-on.helper.registerEvent = (target, eventDomain, callback = function(){}) => {
+		if(target.$appDecorators.on.events[eventDomain]) {
+			throw new Error(`The Event: "${eventDomain}" already exists!`);
+		}
 
-	if(target.$appDecorators.on.events[eventDomain]) {
-		throw new Error(`The Event: "${eventDomain}" already exists!`);
+		// define events
+		target.$appDecorators.on.events[eventDomain] = callback;
+
+		return target;
+
+	},
+	/**
+	 * Helper for registering callbacks
+	 * @param  {String} name
+	 * @param  {Function} target
+	 * @return {Function} callack
+	 */
+	registerCallback: (name, target, callback) => {
+
+		// ucFirst
+		name = name.charAt(0).toUpperCase()+name.slice(1);
+		// its created something like onCreated if passed name=created
+		target[`$on${name}`].on.push(callback);
+
+		return target;
+
+	},
+	/**
+	 * Register namespace of on decorator
+	 * @param  {Function|Objet} target
+	 * @return {Function} target
+	 */
+	registerNamespaces: (target) => {
+
+		// define $appDecorators.on namespace
+		if(!target.$appDecorators) target.$appDecorators = {};
+		if(!target.$appDecorators.on) {
+			target.$appDecorators.on = {
+				events : {},
+			};
+		}
+
+		// define $onCreated.on namespace
+		if(!target.$onCreated) target.$onCreated = {};
+		if(!target.$onCreated.on) target.$onCreated.on = [];
+
+		// define $onDetached.on callbacks
+		if(!target.$onDetached) target.$onDetached = {};
+		if(!target.$onDetached.on) target.$onDetached.on = [];
+
+		// define $onAttached.on callbacks
+		if(!target.$onAttached) target.$onAttached = {};
+		if(!target.$onAttached.on) target.$onAttached.on = [];
+
+		return target;
+
 	}
-
-	// define events
-	target.$appDecorators.on.events[eventDomain] = callback;
-
-	return target;
-
-};
-
-/**
- * Register on created callback
- * @param  {Object} target
- * @param  {Function} callback
- * @return {Function} target
- */
-on.helper.registerOnCreatedCallback = (target, callback) => {
-
-	target.$onCreated.on.push(callback);
-	return target;
-
-};
-
-/**
- * Register on onAttached callback
- * @param  {Object} target
- * @param  {Function} callback
- * @return {Function} target
- */
-on.helper.registerOnAttachedCallback = (target, callback) => {
-
-	target.$onAttached.on.push(callback);
-	return target;
-
-};
-
-/**
- * Register on Detached callback
- * @param  {Object} target
- * @param  {Function} callback
- * @return {Function} target
- */
-on.helper.registerOnDetachedCallback = (target, callback) => {
-
-	// init render engine
-	target.$onDetached.on.push(callback);
-	return target;
-
-};
-
-/**
- * Register namespace of on decorator
- * @param  {Function|Objet} target
- * @return {Function} target
- */
-on.helper.registerNamespaces = (target) => {
-
-	// define $appDecorators.on namespace
-	if(!target.$appDecorators) target.$appDecorators = {};
-	if(!target.$appDecorators.on) {
-		target.$appDecorators.on = {
-			events : {},
-		};
-	}
-
-	// define $onCreated.on namespace
-	if(!target.$onCreated) target.$onCreated = {};
-	if(!target.$onCreated.on) target.$onCreated.on = [];
-
-	// define $onDetached.on callbacks
-	if(!target.$onDetached) target.$onDetached = {};
-	if(!target.$onDetached.on) target.$onDetached.on = [];
-
-	// define $onAttached.on callbacks
-	if(!target.$onAttached) target.$onAttached = {};
-	if(!target.$onAttached.on) target.$onAttached.on = [];
-
-	return target;
-
 };
