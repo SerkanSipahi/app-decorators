c5a4c3d609bc63ec6068e71158627cda79d82c3d
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-06-27 23:09:09 +0200

    src/app-decorators.js: expose libs/*.js

--- src/app-decorators.js
+++ src/app-decorators.js
@@ -1,8 +1,16 @@
 import Bootstrap from './bootstrap';
 
+// Decorators
 export { default as component } from './decorators/component';
 export { default as view } from './decorators/view';
 export { default as style } from './decorators/style';
 export { default as on } from './decorators/on';
 export { default as model } from './decorators/model';
 export { default as modelpool } from './decorators/modelpool';
+
+// Libraries
+export { default as CustomElement } from './libs/customelement';
+export { default as Eventhandler } from './libs/eventhandler';
+export { default as View } from './libs/view';
+export { default as Router } from './apps/router';
+export { default as Pubsub } from './apps/pubsub';
