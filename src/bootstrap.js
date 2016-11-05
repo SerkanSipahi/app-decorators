217495ba23918b8333696019ae8dee5ec62e4138
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-11-05 18:23:33 +0100

    corejs: import only required es6/es7 polyfills

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,15 +1,18 @@
-import { corejs } from './libs/dependencies';
 import { Webcomponents } from './libs/dependencies';
 
-/**
- * Assign polyfills or fix browser bugs
- */
+import {
+    assign,
+    classof,
+    entries,
+    values,
+    getOwnPropertyDescriptors
+} from './libs/dependencies';
 
 /**
  * Assign not supported Object (ES6/ES7) methods
  */
-!Object.assign  ? Object.assign  = corejs.Object.assign  : null;
-!Object.classof ? Object.classof = corejs.Object.classof : null;
-!Object.entries ? Object.entries = corejs.Object.entries : null;
-!Object.values  ? Object.values  = corejs.Object.values  : null;
-!Object.getOwnPropertyDescriptors ? Object.getOwnPropertyDescriptors = corejs.Object.getOwnPropertyDescriptors : null;
+!Object.assign  ? Object.assign  = assign  : null;
+!Object.classof ? Object.classof = classof : null;
+!Object.entries ? Object.entries = entries : null;
+!Object.values  ? Object.values  = values  : null;
+!Object.getOwnPropertyDescriptors ? Object.getOwnPropertyDescriptors = getOwnPropertyDescriptors : null;
--- src/libs/dependencies.js
+++ src/libs/dependencies.js
@@ -1,4 +1,11 @@
-export { default as corejs } from '../../node_modules/core-js/library/index';
+// core-js
+export { default as assign } from '../../node_modules/core-js/library/fn/object/assign';
+export { default as classof } from '../../node_modules/core-js/library/fn/object/classof';
+export { default as entries } from '../../node_modules/core-js/library/fn/object/entries';
+export { default as values } from '../../node_modules/core-js/library/fn/object/values';
+export { default as getOwnPropertyDescriptors } from '../../node_modules/core-js/library/fn/object/get-own-property-descriptors';
+
+// other libs
 export { default as Handlebars } from '../../node_modules/handlebars/dist/handlebars';
 export { default as Webcomponents } from '../../node_modules/webcomponents.js/webcomponents-lite';
 export { default as XRegExp } from '../../node_modules/xregexp/src/index';
