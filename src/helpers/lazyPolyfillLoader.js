f4f6b2de2cdba4182bf0a52472f3777283306437
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-12-04 18:27:30 +0100

    src/helpers/lazyPolyfillLoader.js: add comments

--- src/helpers/lazyPolyfillLoader.js
+++ src/helpers/lazyPolyfillLoader.js
@@ -10,20 +10,24 @@ let promises = null;
  */
 let lazyPolyfillLoader = function(Loader, modules, basePath = '') {
 
+    // when already resolved
     if(promises){
         return promises;
     }
 
+    // determine which polyfills needed
     let _modules = modules.filter(([global, property]) =>
         !global[property]
     );
 
+    // when nothing to do
     if(!_modules.length){
         return Promise.resolve([]);
     }
 
     let _basePath = basePath ? `${basePath}/`: basePath;
 
+    // resolve polyfills
     promises = Promise.all(_modules.map(([global, property, path, assign]) =>
         Loader.import(`${_basePath}${path}`).then(module =>
             !assign ? global[property] = module : null
@@ -35,8 +39,6 @@ let lazyPolyfillLoader = function(Loader, modules, basePath = '') {
     });
 
     return promises;
-
-
 };
 
 export {
