e73ec816e427f534fecc81c089a0fa49f03a4f48
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-01-09 21:34:35 +0100

    src/datas/polyfills.js: add Object.entries

--- src/datas/polyfills.js
+++ src/datas/polyfills.js
@@ -5,6 +5,7 @@
 let polyfills = [
     [Object, 'assign', 'node_modules/core-js/library/fn/object/assign'],
     [Object, 'values', 'node_modules/core-js/library/fn/object/values'],
+    [Object, 'entries', 'node_modules/core-js/library/fn/object/entries'],
     [document, 'registerElement', 'node_modules/webcomponents.js/webcomponents-lite', true],
     [window, 'Reflect', 'node_modules/core-js/library/fn/reflect'],
     [window, 'WeakMap', 'node_modules/core-js/library/fn/weak-map'],
