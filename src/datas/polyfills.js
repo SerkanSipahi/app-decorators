3f972c79b4017d4c92cff5a78ac2f3d1ec50264a
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-01-19 21:12:51 +0100

    src/datas/polyfills.js: improve availability of HTMLElement.prototype

--- src/datas/polyfills.js
+++ src/datas/polyfills.js
@@ -6,7 +6,7 @@ let polyfills = [
     [Object, 'assign', 'node_modules/core-js/library/fn/object/assign'],
     [Object, 'values', 'node_modules/core-js/library/fn/object/values'],
     [Object, 'entries', 'node_modules/core-js/library/fn/object/entries'],
-    [document.body, 'attachShadow', 'jspm_packages/github/webcomponents/shadydom@master/shadydom.min', true],
+    [HTMLElement.prototype, 'attachShadow', 'jspm_packages/github/webcomponents/shadydom@master/shadydom.min', true],
     [document, 'registerElement', 'node_modules/webcomponents.js/webcomponents-lite', true],
     [window, 'Reflect', 'node_modules/core-js/library/fn/reflect'],
     [window, 'WeakMap', 'node_modules/core-js/library/fn/weak-map'],
