4eeb6a9984499ac7166058fed263fb859fad91ed
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-01-31 22:55:15 +0100

    test/libs/stylesheet.spec.js: check for every state the rendered appentTo element

--- test/libs/stylesheet.spec.js
+++ test/libs/stylesheet.spec.js
@@ -1,79 +1,143 @@
 import { Stylesheet } from 'src/libs/stylesheet';
 import sinon from 'sinon';
 
+// init special innerHTML for test
+String.prototype.removeGutter = function(){
+    return this.replace(/[\t\n\r]/gm, '');
+};
+
 describe('Class Stylesheet ', () => {
 
-    let element = null;
-    let defaultOptions = null;
+    describe('initialize', () => {
 
-    beforeEach(() => {
-        element = document.createElement('div');
-        defaultOptions = {
-            appendTo: element,
-            styles: '. foo { color: blue}',
-        };
+        let element = null;
+        let defaultOptions = null;
+
+        beforeEach(() => {
+            element = document.createElement('div');
+            defaultOptions = {
+                appendTo: element,
+                styles: '. foo { color: blue}',
+            };
+        });
+
+        it('should throw error required properties missing', () => {
+
+            let options1 = { appendTo: element };
+            let options2 = { styles: element };
+            let options3 = { appendTo: element, styles: '' };
+            let options4 = { appendTo: true, styles: '. foo { color: blue}' };
+
+            (() => new Stylesheet() ).should.throw('Required: appendTo and styles');
+            (() => new Stylesheet(options1) ).should.throw('Required: appendTo and styles');
+            (() => new Stylesheet(options2) ).should.throw('Required: appendTo and styles');
+            (() => new Stylesheet(options3) ).should.throw('Required: appendTo and styles');
+            (() => new Stylesheet(options4) ).should.throw('Passed appendTo element should be instance of HTMLElement');
+
+        });
+
+        it('wenn kein appendTo, dann als default in <head> ', () => {
+            // in head kann nur angehängt werden wenn mindestens DOMContenLoaded state
+            // erreicht ist, weil dann <head> element gezogen werden kann. Und
+            // dann prüfen was in onAttach on steht
+        });
+
+        it('should not throw when passed arguments correct', () => {
+
+            let options1 = {
+                appendTo: element,
+                styles: '. foo { color: blue}',
+            };
+
+            (() => new Stylesheet(options1) ).should.not.throw();
+        });
+
+        it('should add correct element to instance._scope during the creation', () => {
+
+            // setup
+            let stylesheet = null;
+            let options = null;
+
+            // Test 1
+            options = Object.assign({}, defaultOptions, { attachOn: 'DOMContentLoaded' });
+            stylesheet = new Stylesheet(options);
+            should(stylesheet._scope).be.instanceof(Window);
+
+            // Test 2
+            options = Object.assign({}, defaultOptions, { attachOn: 'load' });
+            stylesheet = new Stylesheet(options);
+            should(stylesheet._scope).be.instanceof(Window);
+
+            // Test 2
+            options = Object.assign({}, defaultOptions, { attachOn: 'some-custom-event' });
+            stylesheet = new Stylesheet(options);
+            should(stylesheet._scope).be.instanceof(HTMLElement);
+
+        });
+
     });
 
-    it('should throw error required properties missing', () => {
+    describe('_insertStylesheet' ,() => {
 
-        let options1 = { appendTo: element };
-        let options2 = { styles: element };
-        let options3 = { appendTo: element, styles: '' };
-        let options4 = { appendTo: true, styles: '. foo { color: blue}' };
+        it('logic inside of _insertStylesheet', () => {
 
-        (() => new Stylesheet() ).should.throw('Required: appendTo and styles');
-        (() => new Stylesheet(options1) ).should.throw('Required: appendTo and styles');
-        (() => new Stylesheet(options2) ).should.throw('Required: appendTo and styles');
-        (() => new Stylesheet(options3) ).should.throw('Required: appendTo and styles');
-        (() => new Stylesheet(options4) ).should.throw('Passed appendTo element should be instance of HTMLElement');
+
+        });
 
     });
 
-    it('should not throw when passed arguments correct', () => {
+    describe('_createStylesheet' ,() => {
 
-        let options1 = {
-            appendTo: element,
-            styles: '. foo { color: blue}',
-        };
+        it('logic inside of _createStylesheet', () => {
 
-        (() => new Stylesheet(options1) ).should.not.throw();
+
+        });
+
     });
 
-    it('should add correct element to instance._scope during the creation', () => {
+    describe('_runProcess' ,() => {
 
-        // setup
-        let stylesheet = null;
-        let options = null;
+        it('logic inside of _runProcess', () => {
 
-        // Test 1
-        options = Object.assign({}, defaultOptions, { attachOn: 'DOMContentLoaded' });
-        stylesheet = new Stylesheet(options);
-        should(stylesheet._scope).be.instanceof(Window);
 
-        // Test 2
-        options = Object.assign({}, defaultOptions, { attachOn: 'load' });
-        stylesheet = new Stylesheet(options);
-        should(stylesheet._scope).be.instanceof(Window);
+        });
 
-        // Test 2
-        options = Object.assign({}, defaultOptions, { attachOn: 'some-custom-event' });
-        stylesheet = new Stylesheet(options);
-        should(stylesheet._scope).be.instanceof(HTMLElement);
+    });
+
+    describe('on' ,() => {
+
+        it('logic inside of on', () => {
+
+
+        });
 
     });
 
     describe('logic inside of _run method', () => {
 
         let stylesheet = null;
-        let element = document.createElement('div');
-        let defaultOptions = {
-            appendTo: element,
-            styles: '. foo { color: blue }',
-        };
+        let element = null;
+        let defaultOptions = null;
+
         let getOptions = options => Object.assign({}, defaultOptions, options);
         let stub = (method, value) => sinon.stub(Stylesheet.prototype, method, _ => value);
 
+        let expectedResult =
+        '<div id="appendToElement">'+
+            '<style type="text/css">'+
+                '.foo { color: blue }'+
+            '</style>'+
+        '</div>';
+
+
         beforeEach(() => {
+
+            element = document.createElement('div');
+            element.id = "appendToElement";
+            defaultOptions = {
+                appendTo: element,
+                styles: '.foo { color: blue }',
+            };
 
             sinon.spy(Stylesheet.prototype, "_isAlreadyDone");
             sinon.spy(Stylesheet.prototype, "_runProcess");
@@ -90,7 +154,7 @@ describe('Class Stylesheet ', () => {
             Stylesheet.prototype._processListener.restore();
             Stylesheet.prototype._addEventListener.restore();
 
-            stylesheet.cleanup();
+            stylesheet.destroy();
 
         });
 
@@ -100,6 +164,8 @@ describe('Class Stylesheet ', () => {
 
         it('should work as expected when "readyState == loading" and "attachOn == DOMContentLoaded"', () => {
 
+            element.innerHTML = '<div class="bar">Hello World</div>';
+
             stub("_getDocumentReadyState", 'loading');
             stylesheet = new Stylesheet(getOptions({ attachOn: 'DOMContentLoaded' }));
             stylesheet._isAlreadyDone.args[0][0].should.be.equal('DOMContentLoaded');
@@ -107,6 +173,21 @@ describe('Class Stylesheet ', () => {
             stylesheet._runProcess.callCount.should.be.equal(0);
             stylesheet._addEventListener.callCount.should.be.equal(1);
             stylesheet._processListener.callCount.should.be.equal(0);
+
+            // simulate DOMContentLoaded event
+            window.dispatchEvent(new Event('DOMContentLoaded'));
+
+            stylesheet._processListener.callCount.should.be.equal(1);
+            stylesheet._runProcess.callCount.should.be.equal(1);
+
+            stylesheet._appendTo.outerHTML.should.be.equal(
+                '<div id="appendToElement">'+
+                    '<style type="text/css">'+
+                        '.foo { color: blue }'+
+                    '</style>'+
+                    '<div class="bar">Hello World</div>'+
+                '</div>'
+            );
 
         });
 
@@ -119,6 +200,14 @@ describe('Class Stylesheet ', () => {
             stylesheet._runProcess.callCount.should.be.equal(0);
             stylesheet._addEventListener.callCount.should.be.equal(1);
             stylesheet._processListener.callCount.should.be.equal(0);
+
+            // simulate load event
+            window.dispatchEvent(new Event('load'));
+
+            stylesheet._processListener.callCount.should.be.equal(1);
+            stylesheet._runProcess.callCount.should.be.equal(1);
+
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
 
         });
 
@@ -136,6 +225,8 @@ describe('Class Stylesheet ', () => {
             stylesheet._addEventListener.callCount.should.be.equal(0);
             stylesheet._processListener.callCount.should.be.equal(0);
 
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
+
         });
 
         it('should work as expected when "readyState == interactive" and "attachOn == load"', () => {
@@ -147,6 +238,14 @@ describe('Class Stylesheet ', () => {
             stylesheet._runProcess.callCount.should.be.equal(0);
             stylesheet._addEventListener.callCount.should.be.equal(1);
             stylesheet._processListener.callCount.should.be.equal(0);
+
+            // simulate load event
+            window.dispatchEvent(new Event('load'));
+
+            stylesheet._processListener.callCount.should.be.equal(1);
+            stylesheet._runProcess.callCount.should.be.equal(1);
+
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
 
         });
 
@@ -164,6 +263,8 @@ describe('Class Stylesheet ', () => {
             stylesheet._addEventListener.callCount.should.be.equal(0);
             stylesheet._processListener.callCount.should.be.equal(0);
 
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
+
         });
 
         it('should work as expected when "readyState == complete" and "attachOn == load"', () => {
@@ -175,6 +276,8 @@ describe('Class Stylesheet ', () => {
             stylesheet._runProcess.callCount.should.be.equal(1);
             stylesheet._addEventListener.callCount.should.be.equal(0);
             stylesheet._processListener.callCount.should.be.equal(0);
+
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
 
         });
 
@@ -189,12 +292,12 @@ describe('Class Stylesheet ', () => {
             stylesheet._processListener.callCount.should.be.equal(0);
             stylesheet._runProcess.callCount.should.be.equal(0);
 
-            window.dispatchEvent(
-                new Event('load')
-            );
+            window.dispatchEvent(new Event('load'));
 
             stylesheet._processListener.callCount.should.be.equal(1);
             stylesheet._runProcess.callCount.should.be.equal(1);
+
+            stylesheet._appendTo.outerHTML.should.be.equal(expectedResult);
 
         });
 
