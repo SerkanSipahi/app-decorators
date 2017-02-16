78021416b0939959f8a8dc4786f2086f39920e73
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-02-16 22:28:43 +0100

    disable shadowDom polyfill

--- src/datas/polyfills.js
+++ src/datas/polyfills.js
@@ -6,7 +6,7 @@ let polyfills = [
     [Object, 'assign', 'node_modules/core-js/library/fn/object/assign'],
     [Object, 'values', 'node_modules/core-js/library/fn/object/values'],
     [Object, 'entries', 'node_modules/core-js/library/fn/object/entries'],
-    [HTMLElement.prototype, 'attachShadow', 'node_modules/@webcomponents/shadydom/shadydom.min.js', true],
+    //[HTMLElement.prototype, 'attachShadow', 'node_modules/shadowdom/shadydom/shadydom.min.js', true],
     [document, 'registerElement', 'node_modules/webcomponents.js/webcomponents-lite', true],
     [window, 'Reflect', 'node_modules/core-js/library/fn/reflect'],
     [window, 'WeakMap', 'node_modules/core-js/library/fn/weak-map'],
