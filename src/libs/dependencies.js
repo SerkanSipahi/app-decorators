0c5136667e26d618da6d65042e577a0a76e7813a
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-03-11 11:40:45 +0100

    fix test

--- src/libs/dependencies.js
+++ src/libs/dependencies.js
@@ -1,3 +1,3 @@
-export { default as HandlebarsRuntime } from '../../node_modules/handlebars/dist/cjs/handlebars.runtime';
+export { default as HandlebarsRuntime } from '../../node_modules/handlebars/dist/handlebars.runtime';
 export { default as RegExp } from '../../node_modules/named-js-regexp/lib/named-js-regexp';
 export { default as extend } from '../../node_modules/extend/index';
