39fc6195e53be75f90f896cfe4ffbe912facf457
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-03-15 20:58:55 +0100

    packages/postcss-parse-atrule-events: improve prototype

--- packages/postcss-parse-atrule-events/src/index.js
+++ packages/postcss-parse-atrule-events/src/index.js
@@ -1,8 +1,24 @@
 import * as postcss from 'postcss';
 
+/**
+ * create Config
+ * @param attachOn {string}
+ * @param styles {string}
+ */
+let createConfig = (attachOn, styles) => ({ attachOn, styles: `${styles} ` });
+
+/**
+ * Determine if node is on root layer
+ * @param node {object}
+ */
 let isRootNode = node => node.parent && /root/.test(node.parent.type);
 
-let stringifyNode = node => {
+/**
+ * Stringify ast node
+ * @param node {object}
+ * @returns {string}
+ */
+let stringifyAstNode = node => {
 
     if(!node || (node && !node.type)) {
         throw new Error('Failed: please pass AST node!');
@@ -14,44 +30,91 @@ let stringifyNode = node => {
     return result;
 };
 
-let stringifyNodes = nodes => {
+/**
+ * Stringify ast nodes
+ * @param nodes {Array}
+ * @returns {string}
+ */
+let stringifyAstNodes = nodes => {
 
     if(!nodes || (nodes && !nodes.length)) {
         throw new Error('Failed: please pass AST collection; array[ast1, ast2, ast3]');
     }
 
     let result = '';
-    nodes.forEach(node => result += stringifyNode(node));
+    nodes.forEach(node => result += stringifyAstNode(node));
 
     return result;
 };
 
-export default postcss.plugin('postcss-reverse-props', _ => {
+/**
+ * Get at rule config
+ * @param atRuleNode {object}
+ * @returns {object|undefined}
+ */
+let getAtRuleConfig = atRuleNode => {
 
-    let walkAtRules = atRuleNode => {
+    if(atRuleNode.name === 'on' && !isRootNode(atRuleNode)){
+        throw new Error('Failed: please use @on at root layer');
+    }
 
-        if(atRuleNode.name === 'on' && !isRootNode(atRuleNode)){
-            throw new Error('Failed: please use @on at root layer');
+    if(!atRuleNode.nodes) {
+        return;
+    }
+
+    let matcher = /\(("|')?([a-z]+)("|')\)/i;
+    let params  = atRuleNode.params;
+    let matched = params.match(matcher);
+    if(!matched) {
+        throw new Error(['Failed: only letters allowed', params]);
+    }
+
+    let atRuleConfig = createConfig(matched[2], stringifyAstNodes(atRuleNode.nodes));
+    return atRuleConfig;
+};
+
+/**
+ * Get rule config
+ * @param ruleNode {object}
+ * @returns {object|undefined}
+ */
+let getRuleConfig = ruleNode => {
+
+    if(!isRootNode(ruleNode)) {
+        return;
+    }
+
+    let styleEvent = createConfig('immediately', stringifyAstNode(ruleNode));
+    return styleEvent;
+};
+
+/**
+ * Parse passed css styles
+ * @param styles {string}
+ * @returns {Array}
+ */
+let parse = styles => {
+
+    let configs = [];
+    let ast = postcss.parse(styles);
+
+    ast.walkAtRules(atRuleNode => {
+        let atRuleconfig = getAtRuleConfig(atRuleNode);
+        if(atRuleconfig) {
+            configs.push(atRuleconfig);
         }
+    });
 
-        if(!atRuleNode.nodes) {
-            return;
+    ast.walkRules(ruleNode => {
+        let ruleConfig = getRuleConfig(ruleNode);
+        if(ruleConfig) {
+            configs.push(ruleConfig);
         }
+    });
 
-        console.log(stringifyNodes(atRuleNode.nodes));
-    };
+    return configs
+};
 
-    let walkRules = ruleNode => {
-
-        if(!isRootNode(ruleNode)) {
-            return;
-        }
-
-        console.log(stringifyNode(ruleNode));
-    };
-
-    return root => {
-        root.walkAtRules(walkAtRules);
-        root.walkRules(walkRules)
-    };
-});
+export {
+    parse,
+}
