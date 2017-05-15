9c099a47f289f0ad208859d93568ecc76622aca8
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-05-15 22:19:06 +0200

    packages/postcss-parse-atrule-events: add core tests

--- packages/postcss-parse-atrule-events/src/index.test.js
+++ packages/postcss-parse-atrule-events/src/index.test.js
@@ -3,80 +3,8 @@ import { parse } from'./index';
 import { it } from './simple-it';
 import 'should';
 
-let stylesFixture = `
-@media on('load') {
-    .foo {
-        color: violette;
-        .bar {
-            color: gray;
-        }
-    }
-    .baz {
-        color: serkan;
-    }
-}
-
-@media on('load') {
-    @import "load/my/styles1.css";
-    .nut {
-        color: magento;
-    }
-}
-@media on('load') {
-    .a {
-        color: red;
-    }
-    .b {
-        color: blue;
-    }
-    @fetch load/my111/styles2.css!async;
-    @fetch load/my111/styles3.css!defer;
-    /*@fetch   <= when no path declared*/
-    /*
-    .serkan {
-        @fetch load/my/styles3.css!defer;
-    }
-    */
-    c. {
-        color: cologne;
-    }
-}
-/**
- * Should not be removed, should be included in "immediately"
- */ 
-@media print {
-    .a {
-        color: yellow;
-        .b {
-            color: b;
-            .c {
-                color: c;
-            }
-        }
-    }
-}
-
-@media rel('preload') {
-    .foo {
-        color: blue;
-    }
-}
-@media rel('preload') {
-	@fetch load/my/styles2.css!async;
-	@fetch load/my/styles3.css!defer;
-}
-
-@media on('myCustomEvent') {
-    .baz {
-        width: 23px;
-    }
-}
-
-#serkan {
-    background-color: liya;
-}
-
-`;
+String.prototype.r = function() { return this.replace( /\s\s+/g, ' ') };
+String.prototype.cs = function(){ return this.replace(/[\r\n]+/g, '').trim() };
 
 it('should create "attachOn: immediately" objects', () => {
 
@@ -127,6 +55,209 @@ it('should create "attachOn: immediately" objects', () => {
         imports: [],
         styles: bazClass,
         type: "default",
+    });
+
+});
+
+it('should create "attachOn: on" with classes', () => {
+
+    let styles =
+        "@media on('load') {"+
+            `.foo {
+                color: violette;
+                .bar {
+                    color: gray;
+                }
+            }
+            .baz {
+                color: serkan;
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "load",
+        imports: [],
+        styles: ".foo { color: violette; } .foo .bar { color: gray; } .baz { color: serkan; }",
+        type: "on",
+    });
+
+});
+
+it('should create "attachOn: on" with classes and @fetch', () => {
+
+    let styles =
+        "@media on('load') {"+
+            `.a {
+                color: red;
+            }
+            .b {
+                color: blue;
+            }
+            @fetch load/my111/styles2.css!async;
+            @fetch load/my111/styles3.css!defer;
+            .c {
+                color: cologne;
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "load",
+        imports: [
+            "load/my111/styles2.css!async",
+            "load/my111/styles3.css!defer",
+        ],
+        styles: ".a { color: red; } .b { color: blue; } .c { color: cologne; }",
+        type: "on",
+    });
+
+});
+
+it('should create "attachOn: on" with classes and @import', () => {
+
+    let styles =
+        "@media on('load') {"+
+            `@import "load/my/styles1.css";
+            .nut {
+                color: magento;
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "load",
+        imports: [],
+        // @Fixme: may be wrong due import need trailing semicolon, but postcss remove it ?!?
+        styles: '@import "load/my/styles1.css" .nut { color: magento; }',
+        type: "on",
+    });
+
+});
+
+it('should create "attachOn: immediately" with classes and @media print', () => {
+
+    let styles =
+        "@media print {"+
+            `.a {
+                color: yellow;
+                .b {
+                    color: b;
+                }
+                .c {
+                    color: c;
+                }
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "immediately",
+        imports: [],
+        styles: "@media print {.a { color: yellow; } .a .b { color: b; } .a .c { color: c; }}",
+        type: "default",
+    });
+
+});
+
+it('should create "attachOn: myCustomEvent" with classes', () => {
+
+    let styles =
+        "@media on('myCustomEvent') {"+
+            `.baz {
+                width: 23px;
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "myCustomEvent",
+        imports: [],
+        styles: ".baz { width: 23px; }",
+        type: "on",
+    });
+
+});
+
+it('should create "attachOn: preload" with only @fetch/s', () => {
+
+    let styles =
+        "@media rel('preload') {"+
+            `@fetch load/my/styles2.css;
+             @fetch load/my/styles3.css;
+            `.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "preload",
+        imports: [
+            "load/my/styles2.css",
+            "load/my/styles3.css",
+        ],
+        styles: "",
+        type: "rel",
+    });
+
+});
+
+it('should create "attachOn: preload" with classes', () => {
+
+    let styles =
+        "@media rel('preload') {"+
+            `.foo {
+                color: blue;
+            }`.r()+
+        "}";
+
+    let result = parse(styles);
+
+    // should have correct length
+    result.should.be.have.length(1);
+
+    // Test 1
+    result[0].styles = result[0].styles.cs(); // remove "new-lines"
+    assert.deepEqual(result[0], {
+        attachOn: "preload",
+        imports: [],
+        styles: ".foo { color: blue; }",
+        type: "rel",
     });
 
 });
