2c69cc0fc359060c18a9a634234fafeb1d8b0f50
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-05-16 21:11:16 +0200

    postcss-parse-atrule-events: refactor: add only fetaure

--- packages/postcss-parse-atrule-events/src/simple-it.js
+++ packages/postcss-parse-atrule-events/src/simple-it.js
@@ -1,20 +1,30 @@
-let its = [];
+let its  = [];
+let only = false;
 
-let it = (description, test) => its.push([description, test]);
-it.skip = () => '';
+let it  = (description, test) => its.push([description, test, false]);
+it.only = (description, test) => its.push([description, test, true]);
+it.skip = () => 1;
 
-it.run  = () => {
-  for(let it of its){
-      let [description, test] = it;
+let testSuccLog = value => console.log(`Test Ok: ${value}`);
+let testFailLog = (value, e) => console.log(`Failed in: ${value}, Message: ${e}`);
+
+it.run = () => {
+
+  if(only) {
+      return;
+  }
+
+  let _its = its.filter(item => item[2]);
+  _its.length && (its = _its);
+
+  for(let [description, test] of its){
       try {
           test();
-          console.log(`Test Ok: ${description}`);
+          testSuccLog(description);
       } catch(e){
-          console.log(`Failed in: ${description}, Message: ${e}`);
+          testFailLog(description, JSON.stringify(e));
       }
   }
 };
 
-export {
-    it,
-}
+export { it }
