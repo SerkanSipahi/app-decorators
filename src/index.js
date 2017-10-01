22a9f069a1cf8297bea04b0b4856855f6bfd79c6
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-10-01 17:24:58 +0200

    add elementToFunction to src/index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Bootstrap
 import './bootstrap';
+import elementToFunction from './libs/element-to-function'
 
 // Libraries
 export { Register } from './libs/customelement';
@@ -9,6 +10,7 @@ export { Stylesheet } from './libs/stylesheet';
 export { Router } from './apps/router';
 export { Pubsub } from './apps/pubsub';
 export { storage } from './libs/random-storage';
+export default elementToFunction;
 
 // Decorators
 export { component } from './decorators/component';
