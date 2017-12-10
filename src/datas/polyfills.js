74eb5295f7e850d5997e5fcdfb324bab2732cfbe
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-12-10 18:51:07 +0100

    Add dom4 for Element.prototype.after

--- src/datas/polyfills.js
+++ src/datas/polyfills.js
@@ -1,17 +1,18 @@
 /**
  * modules
+ * @resources:
+ * - hayato.io/2016/shadowdomv1/
  * @type {*[]}
  */
 let polyfills = [
     [Object, 'assign', 'node_modules/core-js/library/fn/object/assign'],
     [Object, 'values', 'node_modules/core-js/library/fn/object/values'],
     [Object, 'entries', 'node_modules/core-js/library/fn/object/entries'],
-    //[HTMLElement.prototype, 'attachShadow', 'node_modules/shadowdom/shadydom/shadydom.min.js', true],
     [document, 'registerElement', 'node_modules/webcomponents.js/webcomponents-lite', true],
     [window, 'Reflect', 'node_modules/core-js/library/fn/reflect'],
     [window, 'WeakMap', 'node_modules/core-js/library/fn/weak-map'],
     [window, 'Map', 'node_modules/core-js/library/fn/map'],
-    [Element.prototype, 'after', 'node_modules/dom4/build/dom4.js', true],
+    [Element.prototype, 'after', 'node_modules/dom4/build/dom4', true],
 ];
 
 export {
